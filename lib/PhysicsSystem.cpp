a1fdf1305c97a1e2798bdfa7006b9d14b3e71fad
gorgew <78799838+gorgew@users.noreply.github.com>
2022-04-19 16:20:12 -0400

    pre-locator refactor

--- lib/PhysicsSystem.cpp
+++ lib/PhysicsSystem.cpp
@@ -5,7 +5,7 @@
 void PhysicsSystem::process_physics(entt::registry& r, float delta_time) {
     auto view = r.view<position, physics>();
     for (auto [e, pos, phys] : view.each()) {
-        phys.velocity += phys.acceleration;
-        pos.pos += phys.velocity;
+        phys.velocity += (phys.acceleration * delta_time);
+        pos.pos += (phys.velocity * delta_time);
     }
 }
